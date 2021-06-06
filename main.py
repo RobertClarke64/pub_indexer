328d9866c795afa510b5bac7461ee9d166c315c2
Robert Clarke <robert@robertclarke.co.uk>
2021-06-06 20:08:41 +0100

    search and generate distance matrix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from Pub import Pub
 import csv
+import math
 
 def read_pubs(path):
 	pubs = []
 	
-	with open(path, 'r') as file:
+	with open(path, 'r', encoding='utf-8') as file:
 		reader = csv.reader(file)
 		for row in reader:
 			pubs.append(Pub(row[1], row[2], row[3], row[4], row[5]))
@@ -16,6 +17,29 @@ if __name__ == "__main__" :
 
 	pubs = read_pubs("open_pubs.csv")
 	
-	print(pubs[3].get_name())
+	route = []
+	
+	for pub in pubs:
+		if "MILLER" in pub.get_name().upper():
+			route.append(pub)
+			
+	distances = []
+	
+	for start in route:
+		row = []
+		for end in route:
+			start_x = int(start.get_easting())
+			start_y = int(start.get_northing())
+			end_x = int(end.get_easting())
+			end_y = int(end.get_northing())
+			
+			distance = math.sqrt(abs((start_x - end_x)^2 + (start_y - end_y)^2))
+			row.append(distance)
+			
+		distances.append(row)
+		
+		
+	print(distances)
+			
 	
 	
